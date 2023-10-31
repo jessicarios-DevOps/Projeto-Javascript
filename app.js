79c2309a56ccbe73bdc5cc31fdf53578f36f9417
Jéssica Rios <112875350+jessicarios-DevOps@users.noreply.github.com>
2023-10-31 15:32:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -68,8 +68,3 @@ function reiniciarJogo() {
 }
 
 
-// let titulo = document.querySelector('h1');
-// titulo.innerHTML = 'Jogo do número secreto';
-
-// let paragrafo = document.querySelector('p');
-// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
